79b378d823a3dcfe9b3c094a9fba79f966e8f0fb
mmariani2018 <mmariani2018@students.curry.edu>
2021-10-14 22:21:51 -0400

    almost there

--- Mariani.App4/ArrayOfPrimitives.cs
+++ Mariani.App4/ArrayOfPrimitives.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Mariani.App4
@@ -8,19 +9,57 @@ namespace Mariani.App4
     {
         public ArrayOfPrimitives()
         {
-            int[] numbers = new int[50];
-            Console.WriteLine(generateRandomNumbersToAddToArray());
-
+            int[] numbers = CreateTheArray();
+            int tempNumber = generateNumber(); 
+            compareNumberToElements(tempNumber, numbers);
+            showArray(numbers);
+            
         }
-        public int generateRandomNumbersToAddToArray()
+        public int[] CreateTheArray()
         {
-            Random randomNumber = new Random();
-            int tempNumber;
-
-            return tempNumber = randomNumber.Next(51);
-
+           return new int[50];      
         }
-       
+        public int generateNumber()
+        {
+            Random rnd = new Random();
+            int tempNmber = rnd.Next(0, 50);
+
+            return tempNmber;
+        }
+        public void compareNumberToElements(int tempNumber, int[] numbers)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = tempNumber;
+
+                //compare tempNumber to see if it is in array already
+                if (numbers.Contains(tempNumber))
+                {
+                    generateNumber();
+                }
+                else
+                {
+                    for (int n = 0; n < numbers.Length; n++)
+                    {
+                        numbers[n] = tempNumber;
+                    }
+                }
+            }
+            //populate array if number is new
+        }
+        public void showArray(int [] numbers)
+        {
+            //numbers.int.Parse(Console.ReadLine());
+            
+            foreach(int number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+
+
+
+
 
     }
 }
